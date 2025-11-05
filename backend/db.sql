ea8be5045aeef8b69f0482779f912d658d38ac1c
AngelOlan <angel.edoc23@gmail.com>
2025-11-05 15:37:33 -0600

    Add backend files

---
+++ backend/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_usuario VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+);
